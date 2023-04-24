ea98028c19958203f2903eec1798604aca4f0659
Antgenix <alessandro.roethenmund@gmail.com>
2023-04-24 22:02:08 +0200

    chunks

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,32 +1,43 @@
 import { noise } from "../utils/noise";
 import { Block } from "./Block";
 
-export const Ground = () => {
+export const Ground = ({ camera }) => {
 
   const groundSize = 20;
   const cubeSize = 1;
+
+  const chunkSize = 8;
+  const numChunks = groundSize / chunkSize;
   
-const cubes = [];
-for (let x = -groundSize / 2; x < groundSize / 2; x += cubeSize) {
-  for (let z = -groundSize / 2; z < groundSize / 2; z += cubeSize) {
-    const y = noise(x / 20, z / 20);
-    cubes.push(
-      <Block
-        key={`${x}-${y}-${z}`}
-        position={[
-          Math.floor(x) + cubeSize / 2,
-          Math.floor(y) + cubeSize / 2,
-          Math.floor(z) + cubeSize / 2,
-        ]}
-        size={[cubeSize, cubeSize, cubeSize]}
-      />
-    );
+  const chunks = [];
+  for (let cx = 0; cx < numChunks; cx++) {
+    for (let cz = 0; cz < numChunks; cz++) {
+      const chunkX = cx * chunkSize - groundSize / 2;
+      const chunkZ = cz * chunkSize - groundSize / 2;
+      const cubes = [];
+      for (let x = chunkX; x < chunkX + chunkSize; x += cubeSize) {
+        for (let z = chunkZ; z < chunkZ + chunkSize; z += cubeSize) {
+          const y = noise(x / 20, z / 20);
+          cubes.push(
+            <Block
+              key={`${x}-${y}-${z}`}
+              position={[
+                Math.floor(x) + cubeSize / 2,
+                Math.floor(y) + cubeSize / 2,
+                Math.floor(z) + cubeSize / 2,
+              ]}
+              size={[cubeSize, cubeSize, cubeSize]}
+            />
+          );
+        }
+      }
+      chunks.push(<group position={[chunkX, 0, chunkZ]}>{cubes}</group>);
+    }
   }
-}
 
   return (
     <>
-      {cubes}
+      {chunks}
     </>
   );
 };
