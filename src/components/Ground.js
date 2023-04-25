93a8c5fe16362ce820a12d71c1f34d46c6a8b2e5
aro135603 <aro135603@gibb.ch>
2023-04-25 10:47:35 +0200

    Performance improvement

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,5 +1,10 @@
 import { noise } from "../utils/noise";
 import { Block } from "./Block";
+import memoize from 'memoize-one';
+import { useMemo, useCallback } from 'react';
+import React from "react";
+
+const memoizedNoise = memoize(noise)
 
 export const Ground = ({ camera }) => {
 
@@ -10,37 +15,51 @@ export const Ground = ({ camera }) => {
   const numChunks = groundSize / chunkSize;
   
   const maxRenderDistance = 16;
-  const chunks = [];
-  for (let cx = 0; cx < numChunks; cx++) {
-    for (let cz = 0; cz < numChunks; cz++) {
-      const chunkX = cx * chunkSize - groundSize / 2;
-      const chunkZ = cz * chunkSize - groundSize / 2;
-      const distanceToCamera = Math.sqrt(
-        Math.pow(chunkX - camera.x, 2) + 
-        Math.pow(chunkZ - camera.z, 2)
-      );
-      if (distanceToCamera < maxRenderDistance) {
-        const cubes = [];
-        for (let x = chunkX; x < chunkX + chunkSize; x += cubeSize) {
-          for (let z = chunkZ; z < chunkZ + chunkSize; z += cubeSize) {
-            const y = noise(x / 20, z / 20);
-            cubes.push(
-              <Block
-                key={`${x}-${y}-${z}`}
-                position={[
-                  Math.floor(x) + cubeSize / 2,
-                  Math.floor(y) + cubeSize / 2,
-                  Math.floor(z) + cubeSize / 2,
-                ]}
-                size={[cubeSize, cubeSize, cubeSize]}
-              />
-            );
-          }
-        }
-        chunks.push(<group>{cubes}</group>);
+
+  
+  const MemoizedBlock = React.memo(({x, y, z}) => (
+    <Block
+     key={`${x}-${y}-${z}`}
+     position={[
+      Math.floor(x) + cubeSize / 2,
+      Math.floor(y) + cubeSize / 2,
+      Math.floor(z) + cubeSize / 2,
+     ]}
+     size={[cubeSize, cubeSize, cubeSize]}
+    />
+  ))
+
+  const generateCubes = useCallback((chunkX, chunkZ) => {
+    const cubes = [];
+    for (let x = chunkX; x < chunkX + chunkSize; x += cubeSize) {
+      for (let z = chunkZ; z < chunkZ + chunkSize; z += cubeSize) {
+        const y = memoizedNoise(x / 20, z / 20);
+        cubes.push(
+          <MemoizedBlock x={x} y={y} z={z} />
+        );
       }
     }
-  }
+    return cubes;
+  }, [memoizedNoise, chunkSize, cubeSize]);
+
+  const chunks = useMemo(() => {
+    const chunks = [];
+    for (let cx = 0; cx < numChunks; cx++) {
+      for (let cz = 0; cz < numChunks; cz++) {
+        const chunkX = cx * chunkSize - groundSize / 2;
+        const chunkZ = cz * chunkSize - groundSize / 2;
+        const distanceToCamera = Math.sqrt(
+          Math.pow(chunkX - camera.x, 2) + 
+          Math.pow(chunkZ - camera.z, 2)
+        );
+        if (distanceToCamera < maxRenderDistance) {
+          const cubes = generateCubes(chunkX, chunkZ);
+          chunks.push(<group key={`${cx}-${cz}`}>{cubes}</group>);
+        }
+      }
+    }
+    return chunks;
+  }, [camera.x, camera.z, groundSize, chunkSize, numChunks, maxRenderDistance, generateCubes]);
 
   return (
     <>
@@ -48,3 +67,4 @@ export const Ground = ({ camera }) => {
     </>
   );
 };
+
--- src/utils/noise.js
+++ src/utils/noise.js
@@ -1,56 +1,7 @@
+import noisejs from 'noisejs';
+
+const perlinnoise = new noisejs.Noise(Math.random());
+
 export function noise(x, y) {
-    const floorX = Math.floor(x)
-    const floorY = Math.floor(y)
-
-    const fade = (t) => {
-        return t * t * t * (t * (t * 6 - 15) + 10)
-    }
-
-    const lerp = (a, b, t) => {
-        return a + t * (b - a)
-    }
-
-    const smoothstep = (min, max, value) => {
-        const x = Math.max(0, Math.min(1, (value - min) / (max - min)))
-        return x * x * (3 - 2 * x)
-    }
-
-    const grad = (hash, x, y) => {
-        const gradients = [[1,1], [-1,1], [1,-1], [-1,-1], [1,0], [-1,0], [0,1], [0,-1]]
-        const [gradX, gradY] = gradients[hash % 8]
-        return gradX * (x - floorX) + gradY * (y - floorY)
-    }
-
-    const p = [[0,0], [1,0], [0,1], [1,1]]
-
-    const dot2D = (vec1, vec2) => {
-        return vec1[0] * vec2[0] + vec1[1] * vec2[1]
-    }
-
-    const fadeCurve = (t) => {
-        return t * t * t * (t * (t * 6 - 15) + 10)
-    }
-
-    const interpolate = (x0y0, x1y0, x0y1, x1y1, u, v) => {
-        const lerpX0 = lerp(x0y0, x1y0, fadeCurve(u))
-        const lerpX1 = lerp(x0y1, x1y1, fadeCurve(u))
-        return lerp(lerpX0, lerpX1, fadeCurve(v))
-    }
-
-    let corners = []
-    for (let i = 0; i < 4; i++) {
-        const [px, py] = p[i]
-        const hash = Math.floor(Math.abs(Math.sin(px + 37 * Math.sin(py + 73 * Math.sin(x + y)))))
-
-        corners.push([grad(hash, px, py), px - x, py - y])
-    }
-
-    let values = []
-    for (let i = 0; i < 2; i++) {
-        const [a, b, t] = corners[i]
-        const [c, d, s] = corners[i+2]
-        values.push(interpolate(a, b, c, d, t, s))
-    }
-
-    return interpolate(values[0], values[1], 0, 0, smoothstep(-1, 1, x % 1), smoothstep(-1, 1, y % 1))
-}
+    return perlinnoise.perlin2(x, y);
+  }
