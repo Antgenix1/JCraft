7ce8bca1feadc0a9da92cd29941a780b44e82876
aro135603 <aro135603@gibb.ch>
2023-04-26 14:20:50 +0200

    improvement

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,7 +1,7 @@
 import { noise } from "../utils/noise";
 import { Block } from "./Block";
 import memoize from 'memoize-one';
-import React, { useMemo, useCallback } from 'react';
+import React, { useMemo, useCallback, useState } from 'react';
 
 const memoizedNoise = memoize(noise)
 
@@ -12,23 +12,28 @@ export const Ground = ({ camera }) => {
 
   const chunkSize = 8;
   const numChunks = groundSize / chunkSize;
-  
+
   const maxRenderDistance = 16;
 
-  
+  const [chunkCache, setChunkCache] = useState({});
+
   const MemoizedBlock = React.memo(({x, y, z}) => (
     <Block
-     key={`${x}-${y}-${z}`}
-     position={[
-      Math.floor(x) + cubeSize / 2,
-      Math.floor(y) + cubeSize / 2,
-      Math.floor(z) + cubeSize / 2,
-     ]}
-     size={[cubeSize, cubeSize, cubeSize]}
+      key={`${x}-${y}-${z}`}
+      position={[
+        Math.floor(x) + cubeSize / 2,
+        Math.floor(y) + cubeSize / 2,
+        Math.floor(z) + cubeSize / 2,
+      ]}
+      size={[cubeSize, cubeSize, cubeSize]}
     />
-  ))
+  ));
 
   const generateCubes = useCallback((chunkX, chunkZ) => {
+    const cacheKey = `${chunkX},${chunkZ}`;
+    if (chunkCache[cacheKey]) {
+      return chunkCache[cacheKey];
+    }
     const cubes = [];
     for (let x = chunkX; x < chunkX + chunkSize; x += cubeSize) {
       for (let z = chunkZ; z < chunkZ + chunkSize; z += cubeSize) {
@@ -38,8 +43,10 @@ export const Ground = ({ camera }) => {
         );
       }
     }
-    return cubes;
-  }, [memoizedNoise, chunkSize, cubeSize]);
+    const result = <group key={cacheKey}>{cubes}</group>;
+    setChunkCache({ ...chunkCache, [cacheKey]: result });
+    return result;
+  }, [chunkSize, cubeSize, chunkCache]);
 
   const chunks = useMemo(() => {
     const chunks = [];
@@ -48,17 +55,19 @@ export const Ground = ({ camera }) => {
         const chunkX = cx * chunkSize - groundSize / 2;
         const chunkZ = cz * chunkSize - groundSize / 2;
         const distanceToCamera = Math.sqrt(
-          Math.pow(chunkX - camera.x, 2) + 
+          Math.pow(chunkX - camera.x, 2) +
           Math.pow(chunkZ - camera.z, 2)
         );
         if (distanceToCamera < maxRenderDistance) {
-          const cubes = generateCubes(chunkX, chunkZ);
-          chunks.push(<group key={`${cx}-${cz}`}>{cubes}</group>);
+          const cacheKey = `${chunkX},${chunkZ}`;
+          const cachedChunk = chunkCache[cacheKey];
+          const chunk = cachedChunk || generateCubes(chunkX, chunkZ);
+          chunks.push(chunk);
         }
       }
     }
     return chunks;
-  }, [camera.x, camera.z, groundSize, chunkSize, numChunks, maxRenderDistance, generateCubes]);
+  }, [camera.x, camera.z, groundSize, chunkSize, numChunks, maxRenderDistance, chunkCache, generateCubes]);
 
   return (
     <>
